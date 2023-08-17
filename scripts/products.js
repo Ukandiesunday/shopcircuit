903b43ba890046682c9c23eaee98318210937cdd
ukandiesunday <ukandieudo202@gmail.com>
2023-08-17 14:50:11 +0100

    Add the starting files

---
+++ scripts/products.js
@@ -0,0 +1,240 @@
+const product = [{
+  id:"id1",
+  image:"images/men's-sneakers-khaki.jpg",
+  name:"Men's Breathable Lace up Sneakers - Khaki",
+  priceCent:1193,
+  oldPriceCent:1272,
+  rating:{stars:4.5, count:137},
+  itemLeft:6,
+},
+
+{
+  id:"id2",
+  image:"images/men's-printed-t-shirt.jpg",
+  name:"Men's Printed T-Shirt 4 in 1 Black/White.",
+  priceCent:2206,
+  oldpriceCent:2551,
+  rating:{stars:4.0, count:108},
+  itemLeft:4,
+},
+
+{
+  id:"id3",
+  image:"images/ladies-red-hand-bag.jpg", 
+  name:"Ladies Shoulder Hand Bag - Black & White",
+  priceCent:768,
+  oldpriceCent:813,
+  rating:{stars:4.0, count:47},
+  itemLeft:2,
+},
+
+{
+  id:"id4",
+  image:"images/acoustic-guitar",
+  name:"Acoustic Box Guitar-Black 38",
+  priceCent:3552,
+  oldPriceCent:3652,
+  rating:{
+  stars:4.5, count:22},
+  itemLeft:2,
+},
+
+{
+  id:"id5",
+  image:"images/molten-basket-ball.jpg",
+  name:"Molten NBA Professional Basket Ball",
+  priceCent:15211,
+  oldpricCent:17299, 
+  rating:{stars:4.5, count:29},
+  itemLeft:1,
+  },
+
+{
+  id:"id6", 
+  image:"images/learners-keyboard-piano.jpg", 
+  name:"54 Keys Learners XY-813 Keyboard-piano",
+  priceCent:5186,
+  oldpriceCent:5386,
+  rating:{stars:4.5, count:97},
+  itemLeft:1,
+},
+
+{
+  id:"id7", 
+  image:"images/keyboard-sustain-pedal.jpg", 
+  name:"Sustain Pedal Keyboard Foot Damper piano",
+  priceCent:868,
+  oldpriceCent:911,
+  rating:{stars:4.5, count:5},
+  itemLeft:1,
+},
+
+{
+  id:"id8", 
+  image:"imges/black-leather-bag.jpg", 
+  name:"Pu Women leader Hand Bag-Black",
+  priceCent:506,
+  oldpriceCent:606,
+  rating:{stars:4.0, count:31},
+  itemLeft:1,
+},
+
+{
+  id:"id9", 
+  image:"images/wilson-racket.jpg", 
+  name:"Wilson Lawn Tennis Racket",
+  priceCent:2095,
+  oldpriceCent:2195,
+  rating:{stars:4.5, count:34},
+  itemLeft:2,
+},
+
+{
+  id:"id10", 
+  image:"images/2-racket.jpg", 
+  name:"Basket Ball Bat Racket Outdoor Sports",
+  priceCent:2204,
+  oldpriceCent:2304,
+  rating:{stars:4.5, count:2},
+  itemLeft:3,
+},
+
+{
+  id:"id11", 
+  image:"images/celebrety-outfit.jpg", 
+  name:"Celebrety Outfit Wears ",
+  priceCent:945,
+  oldpriceCent:102,
+  rating:{stars:4.5, count:9},
+  itemLeft:1,
+},
+
+{
+  id:"id12", 
+  image:"images/bue-shoe", 
+  name:"Unisex Blue Shoe for Out door Fits",
+  priceCent:567,
+  oldpriceCent:589,
+  rating:{stars:4.5, count:3},
+  itemLeft:2,
+},
+
+{
+  id:"id13", 
+  image:"images/mobile-assessories.png", 
+  name:"Mobile Assessories ",
+  priceCent:3323,
+  oldpriceCent:3544,
+  rating:{stars:4.0, count:31},
+  itemLeft:8,
+},
+
+{
+  id:"id14", 
+  image:"images/suit-trousers.jpg", 
+  name:"Men's Official Suit Trousers",
+  priceCent:555,
+  oldpriceCent:567,
+  rating:{stars:4.5, count:23},
+  itemLeft:5,
+},
+
+{
+  id:"id15", 
+  image:"images/baby-toys.jpg", 
+  name:"Baby Table Toys  ",
+  priceCent:123,
+  oldpriceCent:132,
+  rating:{stars:4.0, count:11},
+  itemLeft:5,
+},
+
+{
+  id:"id16", 
+  image:"images/women's-standard-kit.jpg", 
+  name:"Women's Casual Standard Kit ",
+  priceCent:434,
+  oldpriceCent:455,
+  rating:{stars:4.5, count:9},
+  itemLeft:1,
+},
+
+{
+  id:"id17", 
+  image:"images/summer-cover.jpg", 
+  name:"Summer Cover For Women -Yellow",
+  priceCent:477,
+  oldpriceCent:588,
+  rating:{stars:4.0, count:4},
+  itemLeft:4,
+},
+
+{
+  id:"id18", 
+  image:"images/white-sneakers.jpg", 
+  name:"Men's Casual Air-Cushion Sneakers-White",
+  priceCent:9452,
+  oldpriceCent:1021,
+  rating:{stars:4.5, count:322},
+  itemLeft:1,
+},
+
+{
+  id:"id19", 
+  image:"images/unisex-white-sneakers.jpg", 
+  name:"Unisex Beautiful White Sneakers ",
+  priceCent:9452,
+  oldpriceCent:1021,
+  rating:{stars:4.5, count:322},
+  itemsLeft:1,
+}
+];
+
+let productHTML = "";
+product.forEach((procuct)=>{
+ productHTML+=` 
+    <div class="product-container">
+
+  <div class="image-container">
+    <img src="${product.image}" alt="khaki-sneakers" class="product-image">
+  </div>
+
+  <div class="product-name">
+      ${product.name} 
+  </div>
+
+  <div class="product-price">${(product.priceCent)/100}</div>
+  <div class="old-price">${(product.oldPriceCent)/100}</div>
+
+  <div class="product-rating-container">
+    <img src="images/product-rating-${(product.rating.stars)*10}.png" alt="ratings" class="rating-stars">
+    <div class="product-rating-count">(${product.rating.count})</div>
+  </div>
+
+  
+  <div class="select-product-quantity">
+
+    <div class="items-left">${itemLeft} items left</div>
+    <select>
+      <option selected value="1">1</option>
+      <option value="1">2</option>
+      <option value="1">3</option>
+      <option value="1">4</option>
+      <option value="1">5</option>
+    </select>
+    <button class="next-product">Next</button>
+
+  </div>
+
+  <div class="shop-chant">uShop Express</div>
+
+  <div class="added-to-cart-message">Product added to cart successfully</div>
+
+  <button class="add-to-cart-btn">Add to cart</button>
+
+  </div>
+ ` 
+})
+
+document.querySelector(".js-product-grid").innerHTML = productHTML;
+console.log(productHTML)
