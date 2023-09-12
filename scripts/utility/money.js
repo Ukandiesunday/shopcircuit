674b04f45aa354788aae7b50b45d8e88447febef
Ukandie Sunday Udo <134134409+Ukandiesunday@users.noreply.github.com>
2023-09-12 12:15:23 +0100

    Update money.js updated

--- scripts/utility/money.js
+++ scripts/utility/money.js
@@ -1,19 +1,19 @@
 // To calculate each product price added to cart
  export function calculateMoney(price){
- return price/100;
+ return price / 100;
 }
 
 //To calculate initial cost summary
   let iprice
 export function calcInitialPrice(initPrice){
   iprice = initPrice;
-  return iprice/100;
+  return iprice / 100;
 }
 
 //To calculate tax
    let tax
 export function calcTax(taxRate){
-  tax = (((taxRate)/100)*0.1).toFixed(2);
+  tax = (((taxRate) / 100) * 0.1).toFixed(2);
   return tax;
  }
 
